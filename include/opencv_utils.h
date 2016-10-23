f0ce01891700885e2dba6a0a9f992eaa0206118a
Dragos Carp <dragoscarp@gmail.com>
2016-10-24 01:00:31 +0200

    First commit

---
+++ include/opencv_utils.h
@@ -0,0 +1,14 @@
+#ifndef _OPENCV_UTILS_H_
+#define _OPENCV_UTILS_H_
+
+#include <opencv2/core/core.hpp>
+
+#include "opencv_generated.h"
+
+namespace ProcessingFlow {
+
+void OpenCVMat_to_Mat();
+
+} // namespace ProcessingFlow
+
+#endif // _OPENCV_UTILS_H_
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#include "opencv_utils.h"
+
+int main()
+{
+    std::cout << "foo\n";
+    return 0;
+}
